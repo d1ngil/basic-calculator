a59d5ee3162c5f2dc2a6642ec86b1afbefd53b35
d1ngil <efebaran_075@hotmail.com>
2025-06-30 13:02:54 +0300

    file deleted 4 no reason

--- random_name_picker.py
+++
@@ -1,8 +0,0 @@
-import random 
-
-random_number = random.randint(0,5)
-
-isimler = ["Defne", "Efe", "Mete", "Hüseyin", "Ali", "Hande"]
-
-print (f"Zardaki sayı: {random_number}")
-print(f"Zardaki isim: {isimler[random_number]}")
